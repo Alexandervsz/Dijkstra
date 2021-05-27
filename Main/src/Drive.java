de753b658733380326950df9185f13db9d7f33d1
Alexandervsz <alexander_5560@hotmail.com>
2021-05-27 13:38:53 +0200

    Added constructor method.

--- Main/src/Drive.java
+++ Main/src/Drive.java
@@ -1,2 +1,5 @@
-public class Drive {
+public class Drive extends Step {
+    public Drive(float pricePerKilometer, float kilometers) {
+        this.setStepSize(Math.round(pricePerKilometer * kilometers));
+    }
 }
