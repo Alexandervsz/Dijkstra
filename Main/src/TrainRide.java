166d247a07b0c32ff419e71e399732b9e67c0339
Alexandervsz <alexander_5560@hotmail.com>
2021-05-27 21:54:01 +0200

    changed the price to something more easy/realistic.

--- Main/src/TrainRide.java
+++ Main/src/TrainRide.java
@@ -1,6 +1,6 @@
 public class TrainRide extends Step {
-    public TrainRide(float pricePerMinute, float time, int stepNumber) {
-        this.setStepSize(Math.round(pricePerMinute * time));
+    public TrainRide(float time, int stepNumber) {
+        this.setStepSize(Math.round(time/3));
         this.setStepNumber(stepNumber);
     }
 }
