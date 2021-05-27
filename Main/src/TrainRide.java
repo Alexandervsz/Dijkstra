c6d0805c898566ab73d1772c96b59f9c29403a6f
Alexandervsz <alexander_5560@hotmail.com>
2021-05-27 13:39:14 +0200

    Added constructor method.

--- Main/src/TrainRide.java
+++ Main/src/TrainRide.java
@@ -1,2 +1,5 @@
-public class TrainRide {
+public class TrainRide extends Step {
+    public TrainRide(float pricePerMinute, float time) {
+        this.setStepSize(Math.round(pricePerMinute * time));
+    }
 }
