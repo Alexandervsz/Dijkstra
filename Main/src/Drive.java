f077307c07dddbb093ebcbd64f480decb18393ea
Alexandervsz <alexander_5560@hotmail.com>
2021-05-27 17:31:28 +0200

    Added stepNumber to constructor.

--- Main/src/Drive.java
+++ Main/src/Drive.java
@@ -1,5 +1,6 @@
 public class Drive extends Step {
-    public Drive(float pricePerKilometer, float kilometers) {
+    public Drive(float pricePerKilometer, float kilometers, int stepNumber) {
         this.setStepSize(Math.round(pricePerKilometer * kilometers));
+        this.setStepNumber(stepNumber);
     }
 }
